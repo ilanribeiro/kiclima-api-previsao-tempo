1349c5f8bb5b69335a84b8721fbd65cf51c5d640
ilanribeiro <ilanribeiro@gmail.com>
2021-05-02 22:23:45 -0300

    Adiciona biblioteca Ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeModule } from './pages/home/home.module';
 import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { BookmarksModule } from './pages/bookmarks/bookmarks.module';
     BrowserModule,
     AppRoutingModule,
     HomeModule,
-    BookmarksModule
+    BookmarksModule,
+    StoreModule.forRoot({})
   ],
   providers: [],
   bootstrap: [AppComponent]
