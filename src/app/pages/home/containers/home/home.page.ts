0e8cf98ff5bf9eeaf37bc29bd6e7b8bd74cec701
ilanribeiro <ilanribeiro@gmail.com>
2021-05-08 22:19:36 -0300

    Completa funcionalidade onToggleBoolmark

--- src/app/pages/home/containers/home/home.page.ts
+++ src/app/pages/home/containers/home/home.page.ts
@@ -5,6 +5,7 @@ import { select, Store } from '@ngrx/store';
 import { Observable, Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
 
+import { Bookmark } from 'src/app/shared/models/bookmark.model';
 import { CityWeather } from 'src/app/shared/models/weather.model';
 import * as fromHomeActions from '../../state/home.actions';
 import * as fromHomeSelectors from '../../state/home.selectors';
@@ -51,5 +52,12 @@ export class HomePage implements OnInit, OnDestroy {
     this.store.dispatch(fromHomeActions.loadCurrentWeather({ query }));
   }
 
-  
+  onToggleBookmark() {
+    const bookmark = new Bookmark();
+    bookmark.id = this.cityWeather.city.id;
+    bookmark.name = this.cityWeather.city.name;
+    bookmark.country = this.cityWeather.city.country;
+    bookmark.coord = this.cityWeather.city.coord;
+  }
+
 }
