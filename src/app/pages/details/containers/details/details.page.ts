c328dd3ff176618ca975aa2fe4c6e9f40d14ef19
ilanribeiro <ilanribeiro@gmail.com>
2021-05-10 07:34:44 -0300

    Adiciona Daily Weather component

--- src/app/pages/details/containers/details/details.page.ts
+++ src/app/pages/details/containers/details/details.page.ts
@@ -1,5 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Observable } from 'rxjs';
+import { Store, select } from '@ngrx/store';
+
+import { CityDailyWeather } from 'src/app/shared/models/weather.model';
+import { AppState } from 'src/app/shared/state/app.reducer';
+import * as fromDetailsActions from '../../state/details.actions';
+import * as fromDetailsSelectors from '../../state/details.selectors';
+
 @Component({
   selector: 'jv-details',
   templateUrl: './details.page.html',
@@ -7,9 +15,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DetailsPage implements OnInit {
 
-  constructor() { };
+  details$: Observable<CityDailyWeather>;
+  loading$: Observable<boolean>;
+  error$: Observable<boolean>;
+
+  constructor(private store: Store<AppState>) {
+  };
 
   ngOnInit(): void {
+    this.store.dispatch(fromDetailsActions.loadWeatherDetails());
+
+    this.details$ = this.store.pipe(select(fromDetailsSelectors.selectDetailsEntity));
+    this.loading$ = this.store.pipe(select(fromDetailsSelectors.selectDetailsLoading));
+    this.error$ = this.store.pipe(select(fromDetailsSelectors.selectDetailsError));
   };
 
 };
